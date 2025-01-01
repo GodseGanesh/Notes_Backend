277c5442e483af9e6a5300ef52d3d9e8ae544fbd
Ganesh <ganeshgodse1902@gmail.com>
2025-01-01 12:58:13 +0530

    configured .gitingore

--- api/views.py
+++ api/views.py
@@ -8,9 +8,13 @@ from .serializers import *
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 
+
+
 @api_view(['POST'])
 def register(request):
+    print(request.data)
     serializer = UserRegistrationSerializer(data=request.data)
+    
     if serializer.is_valid():
         serializer.save()
         return Response({"message": "User registered successfully!"}, status=status.HTTP_201_CREATED)
